924f6b53297943bb9cde18b1ce93e46b9971e7f1
Ivan <114565135+Vanovango@users.noreply.github.com>
2024-10-10 11:02:29 +0300

    task-1_1.py

--- task-1_1.py
+++ task-1_1.py
@@ -1,5 +1,15 @@
-numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-
 # TODO заменить значение пропущенного элемента средним арифметическим
 
-print("Измененный список:", ...)
+lost_el = 0
+sum = 0
+
+for i in range(len(numbers)):
+    if numbers[i] is None:
+        lost_el = i
+    else:
+        sum += numbers[i]
+
+numbers[lost_el] = sum / len(numbers)
+
+
+print("Измененный список:", numbers)

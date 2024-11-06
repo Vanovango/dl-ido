ca0a62b01cb47676ff5f7fb8af616a6cc42833b3
Ivan <114565135+Vanovango@users.noreply.github.com>
2024-11-06 13:54:33 +0300

    Add files via upload

---
+++ Модуль 1/Лабораторная работа 4/task-1.py
@@ -0,0 +1,11 @@
+# TODO решите задачу
+import json
+
+
+def task() -> float:
+    with open("input.json", 'r') as f:
+        data = json.load(f)
+    return sum([i["score"] * i["weight"] for i in data])
+
+
+print(f'{task():.3f}')
